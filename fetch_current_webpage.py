5c7f300f168bf744d87ec47aa437718172df278e
Adel Hassan <sean.dashals@gmail.com>
2021-05-31 18:51:35 -0500

    fetches current webpage

--- fetch_current_webpage.py
+++ fetch_current_webpage.py
@@ -7,11 +7,8 @@ from bs4 import BeautifulSoup
 def fetch_current_webpage():
     r = requests.get('https://www.bcm.edu/education/school-of-medicine/m-d-program/current-students/student-affairs/class-of-2025')
     soup = BeautifulSoup(r.text, features='lxml')
-    f = open('blank.html', 'w')
-    f.write(r.text)
-    res = soup.find_all('div', {"class": "leadsection"})
-    print(res)
-    return ""
+    res = soup.find('div', {"class": "leadsection"})
+    return res
 
 
 # for testing purposes
