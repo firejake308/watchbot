07f5f6cc29505da6c1a1a5c94908141d902b10ed
Sam  Shenoi <samshenoi@Sams-MBP.attlocal.net>
2021-05-31 22:11:54 -0500

    Truncated text

--- generate_message.py
+++ generate_message.py
@@ -1,12 +1,12 @@
 
-
+truncate_size = 1000
 def generate_message(diff_string,website_name):
     message_string = """
      A difference has been detected in the %s website. Please
-     refer to the website for further information. A full unedited description of the change can be seen below.
+     refer to the website for further information. A truncated description of the change can be seen below.
 
      ****************************Change Log********************************
      %s
      **********************************************************************
-    """ % (website_name,diff_string)
+    """ % (website_name,diff_string[0:truncate_size])
     return message_string
