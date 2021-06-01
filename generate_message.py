68441664048a3b81eea69cfec7fdc9103c316b7e
Samuel Shenoi <31999037+shenoisam@users.noreply.github.com>
2021-05-31 21:29:41 -0500

    Update generate_message.py
    Co-authored-by: Adel Hassan <firejake308@users.noreply.github.com>

--- generate_message.py
+++ generate_message.py
@@ -4,7 +4,7 @@ def generate_message(diff_string,website_name):
     message_string = """
     Dear User,
 
-     This message is to inform you that a difference has been detected in the %s website. Please
+     A difference has been detected in the %s website. Please
      refer to the website for further information. A full unedited description of the change can be seen below.
 
      ****************************Change Log********************************
@@ -12,4 +12,3 @@ def generate_message(diff_string,website_name):
      **********************************************************************
     """ % (website_name,diff_string)
     return message_string
-
