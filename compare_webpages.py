30f0314a0cc1974c9e266a6c95e6fc3e7b478dc0
Sam  Shenoi <samshenoi@Sams-MBP.attlocal.net>
2021-06-01 14:03:54 -0500

    Updates

--- compare_webpages.py
+++ compare_webpages.py
@@ -1,12 +1,12 @@
 import difflib as dl
 from DiffLibParser import DifflibParser
 
-RIGHT_ONLY = 2
+VALID_CODES = [1]
 def compare_webpages(list_string1, list_string2):
     diff = DifflibParser(list_string1,list_string2)
 
     # We only care about additions in the new file at this point
-    striped_lines = [d["line"] for d in diff if d["code"] > 0]#RIGHT_ONLY]
+    striped_lines = [d["line"] for d in diff if d["code"] in VALID_CODES]
     changes = "\n".join(striped_lines)
 
 
--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def run_bot():
 
 if __name__ == '__main__':
     client = discord.Client()
+    print(run_bot())
     @client.event
     async def on_ready():
         print('We have logged in as {0.user}'.format(client))
