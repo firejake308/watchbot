839dbb60668e4384fa1dfa0e99c1a1974549b51d
Sam  Shenoi <samshenoi@Sams-MBP.attlocal.net>
2021-05-31 22:15:19 -0500

    Modified message

--- generate_message.py
+++ generate_message.py
@@ -2,8 +2,7 @@
 truncate_size = 1000
 def generate_message(diff_string,website_name):
     message_string = """
-     A difference has been detected in the %s website. Please
-     refer to the website for further information. A truncated description of the change can be seen below.
+     A difference has been detected in the %s website. Please refer to the website for further information. A truncated description of the change can be seen below.
 
      ****************************Change Log********************************
      %s
